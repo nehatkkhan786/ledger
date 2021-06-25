2cf1d5edcf715846da1d9bc91a6f6bab8c36027b
ABDUL RASID <abdulrasid@ABDULs-MacBook-Pro.local>
2021-06-25 21:39:55 +0530

    changed no field to charfield

--- hyledger/models.py
+++ hyledger/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Clients(models.Model):
 	name = models.CharField(max_length=200)
 	balance = models.IntegerField(default=0)
-	phone = models.IntegerField(null=True)
+	phone = models.CharField(max_length=200, null=True, blank=True)
 	firm_name = models.CharField(max_length=255, null=True)
 	email = models.EmailField(null=True)
 
