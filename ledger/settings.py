8a20f391b2d31a15606803a7130d22e69d7efa4c
ABDUL RASID <abdulrasid@ABDULs-MacBook-Pro.local>
2021-06-25 14:52:50 +0530

    added local folder code in setting.py

--- ledger/settings.py
+++ ledger/settings.py
@@ -125,3 +125,20 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+
+try:
+    from .local import *
+except:
+    pass
+
+
+
+
+
+
+
+
